f4179f8cc9f7234558a5cacc03ace18c82d188c7
Nirmalmahen <nirmalmahen@gmail.com>
2016-08-13 12:28:00 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Odd{
+public static void main(String args[]){
+int input;
+Scanner in=new Scanner(System.in);
+input=in.nextInt();
+if((input%2)==0)
+System.out.println("Even");
+else 
+System.out.println("Odd");
+}
+}
